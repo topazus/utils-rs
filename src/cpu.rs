8f31e89dc28d7fa9e74c3ef004fd5a6dcc022c0c
topazus <topazus@outlook.com>
2022-04-30 23:26:38 +0800

    update

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,2 +1,45 @@
 #[derive(Debug, Clone)]
-pub struct CpuInfo {}
+pub struct CpuInfo {
+    processor usize,
+    vendor_id String,
+    cpu_family String,
+    model String,
+    model_name String,
+    stepping String,
+    microcode String,
+    cpu_mhz usize,
+    cache_size usize,
+    physical_id usize,
+    siblings usize,
+    core_id usize,
+    cpu_cores   usize,
+    apicid usize,
+    initial_apicid usize,
+    fpu bool,
+    fpu_exception
+    cpuid_level
+    wp
+    flags
+    vmx_flags
+    bugs
+    bogomips
+    clflush_size
+    cache_alignment
+    address_sizes
+    power_management
+}
+
+/// Split a list into a list of lists
+pub fn split_list<T: std::cmp::PartialEq>(ls: Vec<T>, sep: T) -> Vec<Vec<T>> {
+    let mut result: Vec<Vec<T>> = vec![];
+    let mut temp: Vec<T> = vec![];
+    for x in ls {
+        if x == sep {
+            result.push(temp);
+            temp = vec![];
+        } else {
+            temp.push(x);
+        }
+    }
+    result
+}
--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,22 @@ use std::{
     fs::{self, File},
     os,
 };
+
+use cpu::split_list;
 mod cpu;
 mod disk;
 mod file;
 mod filesystem;
 mod re;
+
 fn main() {
     let contents = std::fs::read_to_string("/proc/cpuinfo").unwrap();
     let mut lines = contents.lines().collect::<Vec<_>>();
-    lines.retain(|&x| x != "");
-    println!("{:?}", lines);
+    let res = split_list(lines, "");
+    let c = res[0]
+        .iter()
+        .map(|&x| x.split(":").nth(0).unwrap().trim())
+        .collect::<Vec<&str>>();
+    c.iter()
+        .for_each(|&x| println!("{}", x.replace(" ", "_").to_lowercase()));
 }
